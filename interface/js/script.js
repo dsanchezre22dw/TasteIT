caead92018ee9b442f16c25c4ee882f5fd0bb515
Jon Seijas <jseijasco22dw@ikzubirimanteo.com>
2023-11-27 13:55:01 +0100

    form added

--- interface/js/script.js
+++ interface/js/script.js
@@ -1,18 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const animatedElements = document.querySelectorAll(".animated-element");
+window.onload = inicio
 
-    function checkScroll() {
-        animatedElements.forEach(element => {
-            const elementPosition = element.getBoundingClientRect().top;
-            const screenHeight = window.innerHeight;
-
-            if (elementPosition < screenHeight * 0.90) {
-                element.classList.add("animate");
-            }
-        });
-    }
-
-    // Llama a la función cuando se carga la página y en cada evento de scroll
+function inicio(){
     checkScroll();
     window.addEventListener("scroll", checkScroll);
-});
+}
+
+
+function checkScroll() {
+    const animatedElements = document.querySelectorAll(".animated-element");
+
+    animatedElements.forEach(element => {
+        const elementPosition = element.getBoundingClientRect().top;
+        const screenHeight = window.innerHeight;
+
+        if (elementPosition < screenHeight * 0.90) {
+            element.classList.add("animate");
+        }
+    });
+
+}
