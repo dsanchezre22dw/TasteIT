d48b9d29548c8e85d720a9a4c8bf9fffd3830a85
dsanchezre22dw <dsanchezre22w@ikzubirimanteo.com>
2023-12-19 07:08:59 +0100

    Login interface

--- Back/TasteIT_Laravel/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ Back/TasteIT_Laravel/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -20,7 +20,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): Response
     {
-        return Inertia::render('Auth/Login', [
+        return Inertia::render('Auth/LoginDavid', [
             'canResetPassword' => Route::has('password.request'),
             'status' => session('status'),
         ]);
---
+++ Back/TasteIT_Laravel/resources/js/Pages/Auth/LoginDavid.jsx
@@ -0,0 +1,155 @@
+import React from "react";
+import { useEffect } from 'react';
+import Checkbox from '@/Components/Checkbox';
+import GuestLayout from '@/Layouts/GuestLayout';
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import { Head, Link, useForm } from '@inertiajs/react';
+
+export default function LoginDavid( { status, canResetPassword } ) {
+  const { data, setData, post, processing, errors, reset } = useForm({
+    email: '',
+    password: '',
+    remember: false,
+  });
+
+  useEffect(() => {
+      return () => {
+          reset('password');
+      };
+  }, []);
+
+  const submit = (e) => {
+    console.log("pringau")
+      e.preventDefault();
+
+      post(route('login'));
+  };
+
+
+
+
+  return (
+    <section className="h-full bg-neutral-200 dark:bg-neutral-700">
+      <div className="container h-full p-10">
+        <div className="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
+          <div className="w-full">
+            <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
+              <div className="g-0 lg:flex lg:flex-wrap">
+                {/* <!-- Left column container--> */}
+                <div className="px-4 md:px-0 lg:w-6/12">
+                  <div className="md:mx-6 md:p-12">
+                    {/* <!--Logo--> */}
+                    <div className="text-center">
+                      <img
+                        className="mx-auto w-48 mb-10"
+                        src="/assets/img/logo.png"
+                        alt="logo"
+                      />
+                    </div>
+
+                    <form onSubmit={submit}>
+                      {/* <!--Email input--> */}
+                      <TextInput
+                          id="email"
+                          type="email"
+                          name="email"
+                          value={data.email}
+                          className="mt-1 block w-full"
+                          autoComplete="username"
+                          onChange={(e) => setData('email', e.target.value)}
+                          placeholder="Email"
+                          required
+                      />
+                      <InputError message={errors.email} className="mt-2" />
+
+                      {/* <!--Password input--> */}
+
+                      <TextInput
+                        id="password"
+                        type="password"
+                        name="password"
+                        value={data.password}
+                        className="my-4 block w-full"
+                        autoComplete="current-password"
+                        onChange={(e) => setData('password', e.target.value)}
+                        placeholder="Password"
+                        required
+                      />
+                      <InputError message={errors.password} className="mt-2" />
+
+                      {/* <!--Forgot password link--> */}
+                      {canResetPassword && (
+                          <Link
+                              href={route('password.request')}
+                              className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                          >
+                              Forgot your password?
+                          </Link>
+                      )}
+
+                      {/* <!--Submit button--> */}
+                      <div className="mb-12 pb-1 pt-1 text-center">
+                        
+                          <button
+                            className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
+                            type="submit"
+                            style={{
+                              background:
+                                "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
+                            }}
+                          >
+                            Log in
+                          </button>
+                        
+                      </div>
+
+                      {/* <!--Register button--> */}
+                      <div className="flex items-center justify-between pb-6">
+                        <p className="mb-0 mr-2">Don't have an account?</p>
+                          <Link
+                                href={route('register')}
+                            >
+                            <button
+                              type="button"
+                              className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                            >
+                              Register
+                            </button>
+                          </Link>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+
+                {/* <!-- Right column container with background and description--> */}
+                <div
+                  className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
+                  style={{
+                    backgroundImage:
+                      `url("/assets/img/background_login.jpg")`,
+                      backgroundSize: 'cover',
+                  }}
+                >
+                  <div className="px-4 py-6 text-white md:mx-6 md:p-12">
+                    <h4 className="mb-6 text-xl font-semibold">
+                      We are more than just a company
+                    </h4>
+                    <p className="text-sm">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
