5437969a7b180f002cb2a82df2e0641c29a3cc5f
David Sanchez <dsanchezre22dw@ikzubirimanteo.com>
2023-12-19 09:59:23 +0100

    Correct Register in route

--- Back/TasteIT_Laravel/app/Http/Controllers/Auth/RegisteredUserController.php
+++ Back/TasteIT_Laravel/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -23,7 +23,7 @@ class RegisteredUserController extends Controller
      */
     public function create(): Response
     {
-        return Inertia::render('Auth/RegisterDavid');
+        return Inertia::render('Auth/Register');
     }
 
     /**
--- Back/TasteIT_Laravel/resources/js/Pages/Auth/Register.jsx
+++ Back/TasteIT_Laravel/resources/js/Pages/Auth/Register.jsx
@@ -176,142 +176,193 @@ export default function Register() {
     }
 
     return (
-        <GuestLayout>
-            <Head title="Register" />
 
-            <form name="form_register" onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="firstname" value="First name*" />
+        <section className="h-full"
+            style={{
+                background: 'linear-gradient(90deg, rgb(204, 152, 152), rgba(121,9,9,1))',
+            }}
+        >
+        <div className="container h-full p-10">
+            <div className="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
+            <div className="w-full">
+                <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
+                <div className="g-0 lg:flex lg:flex-wrap">
+                    {/* <!-- Left column container--> */}
+                    <div className="px-4 md:px-0 lg:w-6/12">
+                    <div className="md:mx-6 md:p-12">
+                        {/* <!--Logo--> */}
+                        <div className="text-center">
+                        <img
+                            className="mx-auto w-48 mb-10"
+                            src="/assets/img/logo.png"
+                            alt="logo"
+                        />
 
-                    <TextInput
-                        id="firstname"
-                        name="firstname"
-                        value={data.firstname}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        onChange={(e) => setData('firstname', e.target.value)}
-                        required
-                        maxLength='50'
-                    />
+                        </div>
 
-                    <InputError message={errorMessages.firstname} className="mt-2" />
-                    <InputError message={errors.firstname} className="mt-2" />
+                        <form name="form_register" onSubmit={submit}>
+                            <div>
+                                <InputLabel htmlFor="firstname" value="First name*" />
 
-                </div>
+                                <TextInput
+                                    id="firstname"
+                                    name="firstname"
+                                    value={data.firstname}
+                                    className="mt-1 block w-full"
+                                    autoComplete="name"
+                                    isFocused={true}
+                                    onChange={(e) => setData('firstname', e.target.value)}
+                                    required
+                                    maxLength='50'
+                                />
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="surname" value="Surname" />
+                                <InputError message={errorMessages.firstname} className="mt-2" />
+                                <InputError message={errors.firstname} className="mt-2" />
 
-                    <TextInput
-                        id="surname"
-                        name="surname"
-                        value={data.surname}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        onChange={(e) => setData('surname', e.target.value)}
-                        maxLength='100'
-                    />
+                            </div>
 
-                    <InputError message={errorMessages.surname} className="mt-2" />          
-                    <InputError message={errors.surname} className="mt-2" />  
-                    
-                </div>
+                            <div className="mt-4">
+                                <InputLabel htmlFor="surname" value="Surname" />
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="username" value="Username*" />
+                                <TextInput
+                                    id="surname"
+                                    name="surname"
+                                    value={data.surname}
+                                    className="mt-1 block w-full"
+                                    autoComplete="name"
+                                    onChange={(e) => setData('surname', e.target.value)}
+                                    maxLength='100'
+                                />
 
-                    <TextInput
-                        id="username"
-                        name="username"
-                        value={data.username}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={(e) => setData('username', e.target.value)}
-                        required
-                        maxLength='50'
-                    />
+                                <InputError message={errorMessages.surname} className="mt-2" />          
+                                <InputError message={errors.surname} className="mt-2" />  
+                                
+                            </div>
 
-                    <InputError message={errors.username} className="mt-2" />
-                </div>
+                            <div className="mt-4">
+                                <InputLabel htmlFor="username" value="Username*" />
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email*" />
+                                <TextInput
+                                    id="username"
+                                    name="username"
+                                    value={data.username}
+                                    className="mt-1 block w-full"
+                                    autoComplete="username"
+                                    onChange={(e) => setData('username', e.target.value)}
+                                    required
+                                    maxLength='50'
+                                />
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="email"
-                        onChange={(e) => setData('email', e.target.value)}
-                        required
-                        maxLength='100'
-                    />
+                                <InputError message={errors.username} className="mt-2" />
+                            </div>
 
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
+                            <div className="mt-4">
+                                <InputLabel htmlFor="email" value="Email*" />
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password*" />
+                                <TextInput
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    value={data.email}
+                                    className="mt-1 block w-full"
+                                    autoComplete="email"
+                                    onChange={(e) => setData('email', e.target.value)}
+                                    required
+                                    maxLength='100'
+                                />
 
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                        required
-                    />
+                                <InputError message={errors.email} className="mt-2" />
+                            </div>
 
-                    <InputError message={errorMessages.password} className="mt-2" />
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
+                            <div className="mt-4">
+                                <InputLabel htmlFor="password" value="Password*" />
 
-                <div id="message">
-                    <p id="letter" className="invalid">A <b>lowercase</b> letter</p>
-                    <p id="capital" className="invalid">A <b>capital uppercase</b> letter</p>
-                    <p id="number" className="invalid">A <b>number</b></p>
-                    <p id="length" className="invalid">Minimum <b>8 characters</b></p>
-                </div>
+                                <TextInput
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    value={data.password}
+                                    title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+                                    className="mt-1 block w-full"
+                                    autoComplete="new-password"
+                                    onChange={(e) => setData('password', e.target.value)}
+                                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                                    required
+                                />
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="Confirm Password*" />
+                                <InputError message={errorMessages.password} className="mt-2" />
+                                <InputError message={errors.password} className="mt-2" />
+                            </div>
 
-                    <TextInput
-                        id="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password_confirmation', e.target.value)}
-                        required
-                    />
+                            <div id="message">
+                                <p id="letter" className="invalid">A <b>lowercase</b> letter</p>
+                                <p id="capital" className="invalid">A <b>capital uppercase</b> letter</p>
+                                <p id="number" className="invalid">A <b>number</b></p>
+                                <p id="length" className="invalid">Minimum <b>8 characters</b></p>
+                            </div>
 
-                    <InputError message={errors.password_confirmation} className="mt-2" />
-                </div>
+                            <div className="mt-4">
+                                <InputLabel htmlFor="password_confirmation" value="Confirm Password*" />
 
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href={route('login')}
-                        className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                                <TextInput
+                                    id="password_confirmation"
+                                    type="password"
+                                    name="password_confirmation"
+                                    value={data.password_confirmation}
+                                    className="mt-1 block w-full"
+                                    autoComplete="new-password"
+                                    onChange={(e) => setData('password_confirmation', e.target.value)}
+                                    required
+                                />
+
+                                <InputError message={errors.password_confirmation} className="mt-2" />
+                            </div>
+
+                            <div className="flex items-center justify-end mt-4">
+                                <Link
+                                    href={route('login')}
+                                    className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                                >
+                                    Already registered?
+                                </Link>
+
+                                <PrimaryButton className="ms-4" disabled={processing}>
+                                    Register
+                                </PrimaryButton>
+                            </div>
+                        </form>
+                    </div>
+                    </div>
+
+                    {/* <!-- Right column container with background and description--> */}
+                    <div
+                    className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none"
+                    style={{
+                        backgroundImage:
+                        `url("/assets/img/background_register.jpg")`,
+                        backgroundSize: 'cover',
+                    }}
                     >
-                        Already registered?
-                    </Link>
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Register
-                    </PrimaryButton>
+                    <div className="px-4 py-6 text-white md:mx-6 md:p-12">
+                        <h4 className="mb-6 text-xl font-semibold">
+                        We are more than just a company
+                        </h4>
+                        <p className="text-sm">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                    </div>
+                    </div>
                 </div>
-            </form>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
 
-        </GuestLayout>
+
 
     );
 }
