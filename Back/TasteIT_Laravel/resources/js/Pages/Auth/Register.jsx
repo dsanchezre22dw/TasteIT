963ffe28ae51f2ce6a0b2b2a1109f6d96da791f1
dsanchezre22dw <dsanchezre22w@ikzubirimanteo.com>
2024-02-09 06:06:04 +0100

    Text removed from login and register

--- Back/TasteIT_Laravel/resources/js/Pages/Auth/Register.jsx
+++ Back/TasteIT_Laravel/resources/js/Pages/Auth/Register.jsx
@@ -214,17 +214,6 @@ export default function Register() {
                         backgroundSize: 'cover',
                     }}
                     >
-                    <div className="px-4 py-6 text-white md:mx-6 md:p-12">
-                        <h4 className="mb-6 text-xl font-semibold">
-                        We are more than just a company
-                        </h4>
-                        <p className="text-sm">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        </p>
-                    </div>
                     </div>
                 </div>
                 </div>
